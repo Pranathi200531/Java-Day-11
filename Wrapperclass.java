1c60bcb0b3c7b73a580d1851e3db406f7d0b3003
Pranathi200531 <jpranathi04@gmail.com>
2024-12-12 14:52:57 +0530

    Add files via upload

---
+++ Wrapperclass.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Wrapperclass{
+    public static void main(String args[]){
+       //Scanner sc=new Scanner(System.in);
+        Integer a=new Integer(90);
+        System.out.println(a);
+        Double s=new Double(89);
+        System.out.println(s);
+    }
+}
