23df63767004ad33214130b6bcb1b36e69ff8983
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-06-03 20:30:20 +0530

    Add files via upload

---
+++ todo3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class todo3{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int mouse=150;
+        int monitor=20000;
+        int Keyword=200;
+        int CPU=10000;
+        int Speaker=700;
+        int Mobile=25000;
+        int Earphone=2000;
+        int Laptop=50000;
+        for(int i=0;i<=3;i++)
+        {
+            System.out.println(i);
+
+        }
+    }
+}
